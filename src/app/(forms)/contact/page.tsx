74833b67345f7b06ec3696617ec831100cb3dace
Andrew Colbourne <andrew.colbourne@outlook.com>
2025-03-31 13:07:36 +0100

    Added title formatting to the contact page.

--- src/app/(forms)/contact/page.tsx
+++ src/app/(forms)/contact/page.tsx
@@ -1,5 +1,17 @@
+// -> Imports -> Libraries
+import { getTranslations } from 'next-intl/server';
+
 // -> Imports -> Types
 import type { NextPage } from 'next';
+
+// -> Imports -> Utils
+import { seoMetadata } from '@/utils/seoMetadata';
+
+export async function generateMetadata() {
+  const t = await getTranslations('Forms.Contact');
+
+  return seoMetadata({ title: t('title'), description: t('description') });
+}
 
 const ContactPage: NextPage = () => {
   return <h1>Contact Page</h1>;
