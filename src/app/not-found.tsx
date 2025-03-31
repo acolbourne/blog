5c4633bba4f660cd2c44fc0b78ac111a84463724
Andrew Colbourne <andrew.colbourne@outlook.com>
2025-03-31 12:35:09 +0100

    Added a simple 404 error page.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+// -> Imports -> Types
+import type { NextPage } from 'next';
+
+const NotFoundPage: NextPage = () => {
+  return <h1>Not Found - Update this page</h1>;
+};
+
+export default NotFoundPage;
