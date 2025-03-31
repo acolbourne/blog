069bfa2aaae196a3e9c75f6e36d8293d613c86b3
Andrew Colbourne <andrew.colbourne@outlook.com>
2025-03-31 12:45:56 +0100

    Added language for the 404 page.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,28 @@
+// -> Imports -> Libraries
+import { useTranslations } from 'next-intl';
+import { getTranslations } from 'next-intl/server';
+
 // -> Imports -> Types
 import type { NextPage } from 'next';
 
+// -> Imports -> Utils
+import { seoMetadata } from '@/utils/seoMetadata';
+
+export async function generateMetadata() {
+  const t = await getTranslations('Errors.404');
+
+  return seoMetadata({ title: t('title'), description: t('description') });
+}
+
 const NotFoundPage: NextPage = () => {
-  return <h1>Not Found - Update this page</h1>;
+  const t = useTranslations('Errors.404');
+
+  return (
+    <div>
+      <h1>{t('title')}</h1>
+      <p>{t('content')}</p>
+    </div>
+  );
 };
 
 export default NotFoundPage;
