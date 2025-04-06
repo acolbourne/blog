4cdc8f59c597d06eff05c0bf315bb5a8ea7bbb1b
Andrew Colbourne <andrew.colbourne@outlook.com>
2025-04-06 18:36:01 +0100

    Added light/dark mode functionality.

--- src/components/Navigation/DarkLightToggle/index.tsx
+++ src/components/Navigation/DarkLightToggle/index.tsx
@@ -1,3 +1,28 @@
+'use client';
+
+// -> Imports -> Libraries
+import { useEffect, useState } from 'react';
+
+// -> Imports -> Components
+import { Button } from '@/components/ui/button';
+
 export const DarkLightToggle: React.FC = () => {
-  return <div>Dark/Light Mode Toggle</div>;
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  useEffect(
+    () =>
+      isDarkMode
+        ? document.documentElement.classList.add('dark')
+        : document.documentElement.classList.remove('dark'),
+    [isDarkMode],
+  );
+
+  return (
+    <Button variant="ghost" onClick={toggleDarkMode}>
+      {isDarkMode ? 'Light' : 'Dark'}
+    </Button>
+  );
 };
