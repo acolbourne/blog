9c9756e7925442b54631a738a30c17ef174becf8
Andrew Colbourne <andrew.colbourne@outlook.com>
2025-04-12 18:35:37 +0100

    Added RSS/JSON links next to the dark/light toggle.

--- src/components/Navigation/DarkLightToggle/index.tsx
+++ src/components/Navigation/DarkLightToggle/index.tsx
@@ -5,7 +5,8 @@ import { useEffect, useState } from 'react';
 
 // -> Imports -> Components
 import { Button } from '@/components/ui/button';
-import { Lightbulb, LightbulbOff } from 'lucide-react';
+import { Brackets, Lightbulb, LightbulbOff, Rss } from 'lucide-react';
+import Link from 'next/link';
 
 export const DarkLightToggle: React.FC = () => {
   const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
@@ -26,6 +27,16 @@ export const DarkLightToggle: React.FC = () => {
       <Button variant="ghost" onClick={toggleDarkMode}>
         {isDarkMode ? <Lightbulb /> : <LightbulbOff />}
       </Button>
+      <Link href="/rss.xml" target="_blank">
+        <Button variant="ghost">
+          <Rss className="text-[#f26522]" />
+        </Button>
+      </Link>
+      <Link href="/posts.json" target="_blank">
+        <Button variant="ghost">
+          <Brackets />
+        </Button>
+      </Link>
     </div>
   );
 };
